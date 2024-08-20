559ab23d55dff0b4bb3c35d305360225b51858a8
Hieu <danghieuyt2@gmail.com>
2024-08-20 22:12:48 +0700

    first commit

---
+++ backend/src/main.ts
@@ -0,0 +1,49 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { ValidationContext } from 'graphql';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // CORS config
+  app.enableCors({
+    origin: 'http://127.0.0.1:5174',
+    credentials: true,
+    // all headers that client are allowed to use
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'X-Requested-With',
+      'apollo-require-preflight',
+    ],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  });
+
+  // Cookie parser
+  app.use(cookieParser());
+
+  // Data validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const formattedErrors = errors.reduce((accumulator, error) => {
+          accumulator[error.property] = Object.values(error.constraints).join(
+            ', ',
+          );
+          return accumulator;
+        }, {});
+        console.log('formattedErrors123', formattedErrors);
+        // return formatted errors being an object with properties mapping to errors
+        throw new BadRequestException(formattedErrors);
+      },
+    }),
+  );
+
+  await app.listen(3000);
+}
+bootstrap();
